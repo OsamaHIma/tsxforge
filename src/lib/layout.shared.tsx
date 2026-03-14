1506e99261c4b787f2b7524b1a24a47d27e3adae
OsamaHIma <osamahima018@gmail.com>
2026-03-14 23:35:13 +0200

    feat: Add shared layout configuration for Fumadocs UI.

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -2,15 +2,15 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'OsamaHIma',
+  repo: 'tsxforge',
   branch: 'main',
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'Tanstack Start',
+      title: 'TsxForge',
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
