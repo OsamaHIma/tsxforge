2ffda66d663e645e97c70b480f31fdf7a37e8ab6
OsamaHIma <osamahima018@gmail.com>
2026-03-21 18:57:17 +0200

    Enhancing Documentation UI

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,5 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { Tabs, Tab } from 'fumadocs-ui/components/tabs';
+import { Steps, Step } from 'fumadocs-ui/components/steps';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
@@ -7,6 +8,8 @@ export function getMDXComponents(components?: MDXComponents) {
     ...defaultMdxComponents,
     Tabs,
     Tab,
+    Steps,
+    Step,
     ...components,
   } satisfies MDXComponents;
 }
